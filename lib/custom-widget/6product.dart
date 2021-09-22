8c078d71bb751d984a6218999d29e66f24563c3d
AndyTheKSquareGroup <90226576+AndyTheKSquareGroup@users.noreply.github.com>
2021-09-22 00:42:37 -0500

    Update 6product.dart

--- lib/custom-widget/6product.dart
+++ lib/custom-widget/6product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class SixthCoffeeOne extends StatelessWidget {
   const SixthCoffeeOne({Key? key}) : super(key: key);
@@ -10,40 +11,175 @@ class SixthCoffeeOne extends StatelessWidget {
           color: Colors.blue,
           width: 7,
         ),
-        borderRadius: BorderRadius.circular(15.0),
+        borderRadius: BorderRadius.circular(14),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            width: 150,
-            height: 350,
+            height: 500,
+            width: 147,
             child: Image.asset("images/fifth.jpg"),
           ),
           Expanded(
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Elegant designed coffe",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin:
+                      EdgeInsets.only(left: 0, top: 10, right: 0, bottom: 10),
+                  child: Text(
+                    "Elegant designed coffee plant desktop decoration",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
-                  Text(
-                    "plant for desktop..",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                  )
-                ],
-              ),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.only(left: 0, top: 10, right: 0, bottom: 10),
+                  child: SizedBox(
+                    height: 20,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(0),
+                              side: BorderSide(color: Colors.grey, width: 2))),
+                      child: Text(
+                        "DECORATION",
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.only(left: 0, top: 10, right: 0, bottom: 10),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        // padding: EdgeInsets.all(9),
+                        child: RatingBar.builder(
+                          initialRating: 3,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                          itemSize: 10,
+                          itemBuilder: (context, _) => Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          onRatingUpdate: (rating) {
+                            print(rating);
+                          },
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(9),
+                        child: Text(
+                          "(245)",
+                          style:
+                              TextStyle(fontSize: 11, color: Colors.grey[400]),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.only(left: 0, top: 10, right: 0, bottom: 10),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(9),
+                        child: Text(
+                          "\$15.99",
+                          style: TextStyle(
+                              fontSize: 21, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "\$20",
+                          style: TextStyle(
+                              decoration: TextDecoration.lineThrough,
+                              fontSize: 14,
+                              color: Colors.grey[400]),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(9),
+                        child: Text(
+                          "42% off",
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.only(left: 0, top: 10, right: 0, bottom: 10),
+                  // padding: EdgeInsets.all(9),
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Text(
+                          "• Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                          style:
+                              TextStyle(fontSize: 8, color: Colors.grey[400]),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "• Lorem ipsum dolor sit amet, consectetur adipiscing elit elit",
+                          style:
+                              TextStyle(fontSize: 8, color: Colors.grey[400]),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.only(left: 0, top: 10, right: 0, bottom: 10),
+                  // padding: EdgeInsets.all(9),
+                  child: SizedBox(
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(0),
+                              side: BorderSide(color: Colors.blue, width: 2))),
+                      child: Text(
+                        "Add to cart",
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Container(
-            padding: EdgeInsets.all(10),
-            child: Icon(
-              Icons.favorite_border_outlined,
-              size: 15,
-              color: Colors.grey[400],
-            ),
+            child: Icon(Icons.favorite_border_outlined),
           )
         ],
       ),
